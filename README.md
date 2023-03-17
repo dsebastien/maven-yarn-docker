aa0fac7f91e2e24d8176e3bfb7ffff8595366593
Sebastien Dubois <seb@dsebastien.net>
2023-03-17 16:20:33 +0100

    Switched install approach for node and extracted dockerfile args

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ Dockerfile which builds an image containing maven and yarn build tools
 
 ## Releasing a new version
 - Make your changes
-- Edit build-and-push.sh to set the new version
+- Edit `build-and-push.sh` to set the new version in `IMAGE_VERSION`
 - Commit/Tag/Push
 - Draft a new release on Github
 - run `./build-and-push.sh` to build the image and push it to DockerHub
+
+## TODO
+Evaluate `alpine-sdk` as replacement for `g++ make gcc`
--- build-and-push.sh
+++ build-and-push.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
-VERSION=1.0.2
-docker build . --tag dsebastien/maven-yarn:${VERSION}
-docker tag dsebastien/maven-yarn:${VERSION} dsebastien/maven-yarn:latest
-docker push dsebastien/maven-yarn:${VERSION}
+IMAGE_VERSION=1.0.3
+
+CORRETTO_DOCKER_IMAGE_VERSION=17-alpine3.17
+
+MAVEN_VERSION=3.9.0
+NODE_VERSION=16.17.1
+YARN_VERSION=1.22.19
+
+docker build . \
+  --build-arg MAVEN_VERSION=${MAVEN_VERSION} \
+  --build-arg NODE_VERSION=${NODE_VERSION} \
+  --build-arg YARN_VERSION=${YARN_VERSION} \
+  --build-arg CORRETTO_DOCKER_IMAGE_VERSION=${CORRETTO_DOCKER_IMAGE_VERSION} \
+  --tag dsebastien/maven-yarn:${IMAGE_VERSION}
+docker tag dsebastien/maven-yarn:${IMAGE_VERSION} dsebastien/maven-yarn:latest
+docker push dsebastien/maven-yarn:${IMAGE_VERSION}
 docker push dsebastien/maven-yarn:latest
